47aaccfbc2f962fae33179b3734830bb015703eb
madsemilgodstil <mads93n2@stud.kea.dk>
2025-02-17 15:28:22 +0100

    dddddddd

--- src/app/pages/products/page.js
+++ src/app/pages/products/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-
-import KitCard from '@/components/kit_card/KitCard'
+import KitCard from '@/components/kitCard/KitCard'
+import ProductCard from '@/components/productCard/ProductCard'
 
 export default function Home () {
   return (
@@ -15,6 +15,58 @@ export default function Home () {
         <KitCard title='Card 4' description='This is card number 4' />
         <KitCard title='Card 5' description='This is card number 5' />
         <KitCard title='Card 6' description='This is card number 6' />
+      </div>
+
+      {/* Grid layout for 6 products */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6 place-items-center mt-10'>
+        <ProductCard
+          title='Sony FX6'
+          description='Professional cinema camera.'
+          price='30,000 DKK'
+          buttonText='Buy Now'
+          buttonLink='/products/sony-fx6'
+          imageSrc='/img/sony_fx6.jpg'
+        />
+        <ProductCard
+          title='Sony FX6'
+          description='Professional cinema camera.'
+          price='30,000 DKK'
+          buttonText='Buy Now'
+          buttonLink='/products/sony-fx6'
+          imageSrc='/img/sony_fx6.jpg'
+        />
+        <ProductCard
+          title='Sony FX6'
+          description='Professional cinema camera.'
+          price='30,000 DKK'
+          buttonText='Buy Now'
+          buttonLink='/products/sony-fx6'
+          imageSrc='/img/sony_fx6.jpg'
+        />
+        <ProductCard
+          title='Sony FX6'
+          description='Professional cinema camera.'
+          price='30,000 DKK'
+          buttonText='Buy Now'
+          buttonLink='/products/sony-fx6'
+          imageSrc='/img/sony_fx6.jpg'
+        />
+        <ProductCard
+          title='Sony FX6'
+          description='Professional cinema camera.'
+          price='30,000 DKK'
+          buttonText='Buy Now'
+          buttonLink='/products/sony-fx6'
+          imageSrc='/img/sony_fx6.jpg'
+        />
+        <ProductCard
+          title='Sony FX6'
+          description='Professional cinema camera.'
+          price='30,000 DKK'
+          buttonText='Buy Now'
+          buttonLink='/products/sony-fx6'
+          imageSrc='/img/sony_fx6.jpg'
+        />
       </div>
     </>
   )
