8d67e5420ffd7970cc5e42b5541d621f27421bd0
madsemilgodstil <mads93n2@stud.kea.dk>
2025-02-19 11:10:52 +0100

    hero test

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Hero from '@/components/hero/Hero_landing'
 
 export default function Home () {
   return (
     <>
+      <Hero />
       <div className='text-center text-xl font-bold my-4'>Hello World</div>
 
       <div className='flex flex-col items-center'>
