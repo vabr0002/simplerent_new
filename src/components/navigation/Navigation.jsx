39063f4f1bc6f05ee5f1d5a57f73be6c0f260fde
madsemilgodstil <mads93n2@stud.kea.dk>
2025-02-20 13:24:20 +0100

    toolbox v1

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
-import { FaSearch, FaCamera } from "react-icons/fa";
+import {
+  FaSearch,
+  FaCamera,
+  FaCalendarAlt,
+  FaUser,
+  FaHeart,
+  FaShoppingCart,
+  FaExclamationCircle,
+} from "react-icons/fa";
 import { BsGearWideConnected } from "react-icons/bs";
 
 const Navigation = () => {
+  const [isToolboxOpen, setIsToolboxOpen] = useState(false);
+
   const iconClasses =
     "text-3xl text-white group-hover:text-lime transition-colors duration-200";
   const linkClasses =
-    "group text-p font-light hover:text-lime py-2 px-4 w-full h-16 text-center flex flex-col items-center justify-center whitespace-nowrap"; // Added px-4
-  const iconButtonClasses =
-    "relative bg-white text-black p-4 w-10 h-10 flex items-center justify-center rounded-xl hover:scale-110 transition-transform duration-300 cursor-pointer hover:bg-lime";
+    "group text-p font-light hover:text-lime py-2 px-4 w-full h-16 text-center flex flex-col items-center justify-center whitespace-nowrap";
 
   return (
-    <nav className="flex flex-col items-center">
-      <div className="flex justify-between items-center w-full p-3">
+    <nav className="flex flex-col items-center relative w-full">
+      {/* Top Navigation */}
+      <div className="flex justify-between items-center w-full p-3 bg-black text-white">
         <div className="flex gap-6 ml-4">
           <Link className="text-h4 hover:text-lime" href="">
             How it works
@@ -25,16 +35,59 @@ const Navigation = () => {
         <div>
           <h1>LOGO</h1>
         </div>
-        <div className="flex gap-6 mr-4">
-          <div className={iconButtonClasses}>
+        <div className="flex gap-6 mr-4 relative">
+          {/* Search Icon - Stays White */}
+          <div className="relative bg-white text-black p-4 w-10 h-10 flex items-center justify-center rounded-xl hover:scale-110 transition-transform duration-300 cursor-pointer hover:bg-lime">
             <FaSearch className="absolute text-black text-xl" />
           </div>
-          <div className={iconButtonClasses}>
+
+          {/* Gear Icon - Turns Lime When Active */}
+          <div
+            className={`relative p-4 w-10 h-10 flex items-center justify-center rounded-xl hover:scale-110 transition-transform duration-300 cursor-pointer ${
+              isToolboxOpen
+                ? "bg-lime text-black"
+                : "bg-white text-black hover:bg-lime"
+            }`}
+            onClick={() => setIsToolboxOpen(!isToolboxOpen)}
+          >
             <BsGearWideConnected className="absolute text-black text-xl" />
           </div>
+
+          {/* Floating Toolbox */}
+          {isToolboxOpen && (
+            <div className="absolute right-0 top-16 bg-black text-white p-4 rounded-lg shadow-lg transition-all duration-300 z-20 w-16">
+              <div className="grid grid-cols-1 gap-3">
+                {/* Booking Date */}
+                <button className="flex flex-col items-center p-2 bg-white rounded-md hover:bg-lime transition">
+                  <FaCalendarAlt className="text-black text-xl" />
+                </button>
+
+                {/* Profile */}
+                <button className="flex flex-col items-center p-2 bg-white rounded-md hover:bg-lime transition">
+                  <FaUser className="text-black text-xl" />
+                </button>
+
+                {/* Liked Equipment */}
+                <button className="flex flex-col items-center p-2 bg-white rounded-md hover:bg-lime transition">
+                  <FaHeart className="text-black text-xl" />
+                </button>
+
+                {/* Shopping Cart */}
+                <button className="flex flex-col items-center p-2 bg-white rounded-md hover:bg-lime transition">
+                  <FaShoppingCart className="text-black text-xl" />
+                </button>
+
+                {/* Contact */}
+                <button className="flex flex-col items-center p-2 bg-white rounded-md hover:bg-lime transition">
+                  <FaExclamationCircle className="text-black text-xl" />
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
 
+      {/* Category Menu */}
       <div className="flex flex-row w-full pt-2 bg-opacity-80 font-helvetica">
         {[
           "Kits",
