7afadfb78e8435c6286d5bef628a342b64430ed0
vabr0002 <vabr0002@stud.kea.dk>
2025-02-27 15:19:44 +0100

    margintingpåsiderne

--- src/app/pages/howItWorks/page.js
+++ src/app/pages/howItWorks/page.js
@@ -3,7 +3,7 @@ import Accordion from "@/components/accordion/Accordion";
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center mt-10 mb-6">
+      <div className="flex justify-center mt-32 mb-10">
         <h1 className="text-h1">How it Works</h1>
       </div>
       <Accordion />
--- src/app/pages/learn/page.js
+++ src/app/pages/learn/page.js
@@ -2,8 +2,8 @@ import Articles from "@/components/articles/Articles";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex justify-center mt-10 mb-6">
+    <div className="mb-20">
+      <div className="flex justify-center mt-32 mb-10">
         <h1 className="text-h1">Guides & Articles</h1>
       </div>
       <Articles />
