7546d251ad3eaf5bde6d4622c7136f31bf7a257f
madsemilgodstil <mads93n2@stud.kea.dk>
2025-03-18 10:46:28 +0100

    Remove unnecessary whitespace in ProductPage component

--- src/app/pages/products/page.js
+++ src/app/pages/products/page.js
@@ -19,7 +19,7 @@ export default function ProductPage() {
             {/* Product Grid - 4 cards per row on desktop, responsive on smaller screens */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
              
-           < ProductCard />
+           
              
             </div>
             
