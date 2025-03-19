8161da525dc4bb074218d02c2002cb505792884b
madsemilgodstil <mads93n2@stud.kea.dk>
2025-03-19 11:36:35 +0100

    Set default props for ProductCard component and improve styling

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -4,27 +4,27 @@ import React from "react";
 import Button from "../button/Button";
 
 const ProductCard = ({
-  title,
-  description,
-  price,
-  imageSrc,
+  title = "Product Title",
+  description = "Product Description",
+  price = "Product Price",
+  imageSrc = "/img/hero.webp",
   buttonText = "Reserve",
-  buttonLink = "/pages/singleProduct"
+  buttonLink = "/pages/singleProduct",
 }) => {
   return (
-    <div className=" p-6 rounded-lg border-2 hover:scale-105 hover:shadow-md ">
-      <div className="relative w-full h-[160px] mb-6">
+    <div className="p-6 rounded-lg border-2 hover:shadow-md hover:scale-105 group transition-transform duration-300">
+      <div className="relative w-full h-[160px] mb-6 overflow-hidden">
         <Image
           src={imageSrc}
           alt={title}
           width={240}
           height={160}
-          className="w-full h-full object-cover rounded"
+          className="w-full h-full object-cover rounded transition-transform duration-500 group-hover:scale-110"
         />
       </div>
-      <h3 className="text-lg text-black font-semibold ">{title}</h3>
-      <p className="text-sm text-black mt-6 ">{description}</p>
-      <p className="text-base text-black font-bold ">{price}</p>
+      <h3 className="text-lg text-black font-semibold">{title}</h3>
+      <p className="text-sm text-black mt-6">{description}</p>
+      <p className="text-base text-black font-bold">{price}</p>
       <div className="flex justify-between items-center mt-6">
         <Button
           href={buttonLink}
